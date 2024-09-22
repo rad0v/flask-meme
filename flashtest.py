dc85fa07ffad606da7e4b3f245daa4f71ae80fa7
rad0v <roxychan@proton.me>
2024-09-23 00:03:32 +0530

    Add files via upload

--- flashtest.py
+++ flashtest.py
@@ -1,14 +1,21 @@
 #!/bin/python
 
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
+def memes():
+	url = "https://www.reddit.com/r/api/meme/random.json"
+	response = json.loads(requests.request("GET",url).text)
+	meme_large = response["preview"][-2]
+	return meme_large,subreddit
+
 @app.route("/")
-
 def index():
-	return "I will drink Coffee!!!"
-
+	meme_pic,subreddit = memes()
+	return render_template("index.html",meme_pic=meme_pic,subreddit=subreddit)
 
 app.run(host = "0.0.0.0", port=80)
 
